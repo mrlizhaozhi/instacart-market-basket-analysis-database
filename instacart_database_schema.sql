d6ad8a4a5c034343ea99c98947f4d129f851755a
Li Zhao-Zhi <lizhaozhi2017@163.com>
2026-03-22 11:40:38 +0800

    Update instacart_database_schema.sql

--- instacart_database_schema.sql
+++ instacart_database_schema.sql
@@ -2,7 +2,6 @@
 -- 1. Create Database
 -- =========================================
 CREATE DATABASE instacart;
-
 -- NOTE: You cannot use IF NOT EXISTS with CREATE DATABASE in older PostgreSQL versions.
 -- Run this manually if needed, then connect:
 
@@ -91,9 +90,17 @@ FROM 'D:\LZMyDocs\Financial Crime Risk Analytics\Portfolio Projects\InstaCart On
 DELIMITER ','
 CSV HEADER;
 
+-- =========================================
+-- 4. Create Unified Fact Table
+-- =========================================
+
+CREATE TABLE IF NOT EXISTS order_products AS
+SELECT * FROM order_products_prior
+UNION ALL
+SELECT * FROM order_products_train;
 
 -- =========================================
--- 4. Add Constraints (Optional but Recommended)
+-- 5. Add Constraints and Relationships
 -- =========================================
 
 DO $$
@@ -134,16 +141,6 @@ BEGIN
         FOREIGN KEY (product_id) REFERENCES products(product_id);
     END IF;
 END $$;
-
-
--- =========================================
--- 5. Create Unified Fact Table
--- =========================================
-
-CREATE TABLE IF NOT EXISTS order_products AS
-SELECT * FROM order_products_prior
-UNION ALL
-SELECT * FROM order_products_train;
 
 -- =========================================
 -- DONE
